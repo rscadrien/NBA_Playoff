cd422d7ac6d327c3f17c5215c07f8d528efdfa2e
Adrien Devolder <adridevolder@hotmail.com>
2025-07-22 11:57:24 -0400

    Retrain the model

--- Model/model.py
+++ Model/model.py
@@ -4,7 +4,7 @@ import joblib
 from xgboost import XGBClassifier
 
 def train_model(X_train, y_train):
-    # rf_model = RandomForestClassifier(class_weight='balanced', n_estimators=500, random_state=42)
+    #rf_model = RandomForestClassifier(class_weight='balanced', n_estimators=500, random_state=42)
     base_model = XGBClassifier(use_label_encoder=False, eval_metric='logloss')
     chain = ClassifierChain(base_model, order=[0, 1, 2, 3])
     chain.fit(X_train, y_train)
