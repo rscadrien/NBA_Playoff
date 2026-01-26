83de00e9d18a4f2021909e285a26a0a45ee9a6ed
Adrien Devolder <adridevolder@hotmail.com>
2026-01-26 15:39:15 -0500

    Improved streamlit app

--- streamlit_playoff_simulation.py
+++ streamlit_playoff_simulation.py
@@ -7,33 +7,26 @@ from Data_loading_preprocessing.preprocessing import scale_features
 
 # Streamlit UI
 st.title("🏀 NBA Playoff Prediction")
-#User selection of the sorted score
-labels = ['Conf. Semi-Finalist', 'Conf. Finalist', 'NBA Finalist', 'NBA Champion']
-# Regularize the probabilities with a temperature scaling
-sort_col = st.selectbox("Select the score to sort the teams:", labels)
-ascending = st.toggle("Ascending order", value=False)
-
-#Read the current NBA season data
-X_ini = pd.read_csv('./Data/Current_NBA_Season.csv')
-X = X_ini.copy()
-teams = X['Team']
-# Encode and scale input
-X = encode_conference(X, 'encoder_conference.joblib', mode='eval')
-X = encode_playoff_results(X)
-X = drop_columns(X)
-scaling_cols = ['Conf. Seed', 'NBA Seed', 'ORtg Rank', 'DRtg Rank']
-X = scale_features(X, scaling_cols, 'scaler_seed_rank.joblib', mode='eval')
-# Load model and predict
-model = joblib.load('NBA.joblib')
-y_prob = model.predict_proba(X)
-
-T=3
-prob_scaled = (y_prob**(1/T))/((y_prob**(1/T))+((1-y_prob)**(1/T)))
-
-df = pd.DataFrame(prob_scaled, columns=labels, index=teams)
-df.index.name = "Team"
-
-#Sort dataframe based on user selection
-df_sorted = df.sort_values(by=sort_col, ascending=ascending)
-# Display
-st.dataframe(df_sorted, use_container_width=True)
+if st.button("Predict Global Playoff Outcomes"):
+    labels = ['Conf. Semi-Finalist', 'Conf. Finalist', 'NBA Finalist', 'NBA Champion']
+    #Read the current NBA season data
+    X_ini = pd.read_csv('./Data/Current_NBA_Season.csv')
+    X = X_ini.copy()
+    teams = X['Team']
+    # Encode and scale input
+    X = encode_conference(X, 'encoder_conference.joblib', mode='eval')
+    X = encode_playoff_results(X)
+    X = drop_columns(X)
+    scaling_cols = ['Conf. Seed', 'NBA Seed', 'ORtg Rank', 'DRtg Rank']
+    X = scale_features(X, scaling_cols, 'scaler_seed_rank.joblib', mode='eval')
+    # Load model and predict
+    model = joblib.load('NBA.joblib')
+    y_prob = model.predict_proba(X)
+    # Regularize the probabilities with a temperature scaling
+    T=3
+    prob_scaled = (y_prob**(1/T))/((y_prob**(1/T))+((1-y_prob)**(1/T)))
+    #Convert  the results in a dataframe
+    df = pd.DataFrame(prob_scaled, columns=labels, index=teams)
+    df.index.name = "Team"
+    # Display
+    st.dataframe(df, use_container_width=True)
