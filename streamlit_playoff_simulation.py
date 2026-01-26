ab479efc233c9cb0b400ce42e5487fefa9866385
Adrien Devolder <adridevolder@hotmail.com>
2026-01-26 15:22:56 -0500

    Improved streamlit app

--- streamlit_playoff_simulation.py
+++ streamlit_playoff_simulation.py
@@ -7,13 +7,18 @@ from Data_loading_preprocessing.preprocessing import scale_features
 
 # Streamlit UI
 st.title("🏀 NBA Playoff Prediction")
+#User selection of the sorted score
+labels = ['Conf. Semi-Finalist', 'Conf. Finalist', 'NBA Finalist', 'NBA Champion']
+# Regularize the probabilities with a temperature scaling
+sort_col = st.selectbox("Select the score to sort the teams:", labels)
+ascending = st.toggle("Ascending order", value=False)
 
 #Read the current NBA season data
 X_ini = pd.read_csv('./Data/Current_NBA_Season.csv')
 X = X_ini.copy()
 teams = X['Team']
 # Encode and scale input
-X = encode_conference(X_ini, 'encoder_conference.joblib', mode='eval')
+X = encode_conference(X, 'encoder_conference.joblib', mode='eval')
 X = encode_playoff_results(X)
 scaling_cols = ['Conf. Seed', 'NBA Seed', 'ORtg Rank', 'DRtg Rank']
 X = scale_features(X, scaling_cols, 'scaler_seed_rank.joblib', mode='eval')
@@ -21,19 +26,15 @@ X = scale_features(X, scaling_cols, 'scaler_seed_rank.joblib', mode='eval')
 model = joblib.load('NBA.joblib')
 y_prob = model.predict_proba(X)
 
-labels = ['Conf. Semi-Finalist', 'Conf. Finalist', 'NBA Finalist', 'NBA Champion']
-# Regularize the probabilities with a temperature scaling
 T=3
 prob_scaled = (y_prob**(1/T))/((y_prob**(1/T))+((1-y_prob)**(1/T)))
 
 df = pd.DataFrame(prob_scaled, columns=labels, index=teams)
 df.index.name = "Team"
 
-#User selection of the sorted score
-sort_col = st.selectbox("Select the score to sort the teams:", labels)
-ascending = st.toggle("Ascending order", value=False)
-
 #Sort dataframe based on user selection
 df_sorted = df.sort_values(by=sort_col, ascending=ascending)
 # Display
 st.dataframe(df_sorted, use_container_width=True)
+
+
