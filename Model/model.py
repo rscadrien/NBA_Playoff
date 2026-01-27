5a01c98803c63048ea5f9605233d3b9ba712ce86
Adrien Devolder <adridevolder@hotmail.com>
2026-01-27 11:56:04 -0500

    Back to XGB model

--- Model/model.py
+++ Model/model.py
@@ -6,9 +6,9 @@ from xgboost import XGBClassifier
 
 def train_model(X_train, y_train):
     #rf_model = RandomForestClassifier(class_weight='balanced', n_estimators=500,max_depth= 8,min_samples_leaf = 20, max_features = 0.5)
-    #xgb_model = XGBClassifier(eval_metric='logloss')
-    base_model = LogisticRegression(class_weight='balanced', max_iter=1000)
-    chain = ClassifierChain(base_model, order=[0, 1, 2, 3])
+    xgb_model = XGBClassifier(eval_metric='logloss')
+    #base_model = LogisticRegression(class_weight='balanced', max_iter=1000)
+    chain = ClassifierChain(xgb_model, order=[0, 1, 2, 3])
     chain.fit(X_train, y_train)
     return chain
 
