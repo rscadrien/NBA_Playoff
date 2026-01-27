4a08652d6c1350acee7f40ac7f4f512ca208434a
Adrien Devolder <adridevolder@hotmail.com>
2026-01-27 07:54:55 -0500

    Improved app

--- streamlit_playoff_simulation.py
+++ streamlit_playoff_simulation.py
@@ -24,7 +24,6 @@ st.subheader("Run Playoff simulations:")
 T = st.number_input("Upset factor (higher = more upsets)", min_value=1.0, max_value=4.0, step=0.5, value=2.0)
 N = st.number_input("Number of simulations to run", min_value=10, max_value=1000, step=1, value=100)
 
-
 if st.button("Run Playoff Simulations"):
     X = X_ini.copy()
     # Encode and scale
