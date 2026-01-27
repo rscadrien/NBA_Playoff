3070fb959487435d7e0353c154d062a118f88282
Adrien Devolder <adridevolder@hotmail.com>
2026-01-27 11:27:25 -0500

    Sorted the data by season record

--- streamlit_playoff_simulation.py
+++ streamlit_playoff_simulation.py
@@ -17,7 +17,8 @@ st.write("Current NBA Teams Data:")
 X_ini, model = load_data_and_model()
 teams = X_ini['Team'].tolist()
 st.session_state['X_ini'] = X_ini
-st.dataframe(st.session_state['X_ini'], use_container_width=True)
+st.dataframe(st.session_state['X_ini'].sort_values(by='Season Record', ascending=True), 
+             use_container_width=True)
 
 # ---------- Run Playoff Simulations ----------
 st.subheader("Run Playoff simulations:")
