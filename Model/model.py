630ccc4933b32d41319cd54be777f9594300c5dd
Adrien Devolder <adridevolder@hotmail.com>
2026-01-27 10:58:14 -0500

    New training of model

--- Model/model.py
+++ Model/model.py
@@ -4,9 +4,9 @@ import joblib
 from xgboost import XGBClassifier
 
 def train_model(X_train, y_train):
-    rf_model = RandomForestClassifier(class_weight='balanced', n_estimators=500, random_state=42)
-    #base_model = XGBClassifier(eval_metric='logloss')
-    chain = ClassifierChain(rf_model, order=[0, 1, 2, 3])
+    #rf_model = RandomForestClassifier(class_weight='balanced', n_estimators=500, random_state=42)
+    base_model = XGBClassifier(eval_metric='logloss')
+    chain = ClassifierChain(base_model, order=[0, 1, 2, 3])
     chain.fit(X_train, y_train)
     return chain
 
