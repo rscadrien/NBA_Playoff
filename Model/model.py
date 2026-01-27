78a46b809e73b8c4ea12317593203e7f5d7718b5
Adrien Devolder <adridevolder@hotmail.com>
2026-01-27 11:12:19 -0500

    Test with logistic regression

--- Model/model.py
+++ Model/model.py
@@ -1,11 +1,13 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.multioutput import ClassifierChain
+from sklearn.linear_model import LogisticRegression
 import joblib
 from xgboost import XGBClassifier
 
 def train_model(X_train, y_train):
     #rf_model = RandomForestClassifier(class_weight='balanced', n_estimators=500, random_state=42)
-    base_model = XGBClassifier(eval_metric='logloss')
+    #xgb_model = XGBClassifier(eval_metric='logloss')
+    base_model = LogisticRegression(class_weight='balanced', max_iter=1000)
     chain = ClassifierChain(base_model, order=[0, 1, 2, 3])
     chain.fit(X_train, y_train)
     return chain
